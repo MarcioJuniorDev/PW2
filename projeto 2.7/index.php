57a87449ea0146754493f7f86338142d5664c208
MarcioJuniorDev <marciojunior2911@gmail.com>
2025-08-19 15:41:05 -0300

    cadastrando usuario

---
+++ projeto 2.7/index.php
@@ -0,0 +1,34 @@
+<?php
+// dependencias
+// "require" quebra se o arquivo n existir, "include" continua a execução
+require_once 'vendor/autoload.php';
+
+// endereço do site
+const URL = "http://localhost";
+
+// cria o roteador
+$roteador = new CoffeeCode\Router\Router(URL);
+
+// controlador: "cerébro do seu projeto". Gerencia o backend
+// local do controlador
+$roteador -> namespace("Etec\Marcio\Controller");
+
+// rota principal
+$roteador -> group(null);
+$roteador -> get("/", "Principal:inicio");
+$roteador -> get("/sobre", "Principal:sobre");
+$roteador -> get("/login", "Principal:login");
+$roteador -> post("/login", "Principal:autenticar");
+
+// admin
+$roteador -> group("adm");
+$roteador -> get("/cadastro", "adm:exibirFormulariodeCadastrodeUsuario");
+$roteador -> post("/cadastro", "adm:cadastrarUsuario");
+$roteador -> get("/lista", "adm:listarUsuarios");
+$roteador -> get("/login", "adm:login");
+$roteador -> post("/login", "adm:autenticar");
+$roteador -> get("/produtos", "adm:login");
+
+    
+
+$roteador -> dispatch();
